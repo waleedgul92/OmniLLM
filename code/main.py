9a1eb7a2a0489c2631201305cd02260e1fc253de
waleedgul92 <hwaleed0035@google.com>
2024-10-14 13:10:36 +0500

    added one image and text model

--- code/main.py
+++ code/main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from PIL import Image
+from dotenv import load_dotenv
+from text_models import google_genai_text
+from image_models import google_genai_image
 
 # Configure the Streamlit page
 st.set_page_config("Omni LLM", initial_sidebar_state="collapsed")
@@ -64,13 +67,26 @@ st.markdown(
     .sidebar {
         animation: fadeIn 1s ease forwards; /* Apply animation to sidebar itself */
     }
+    
     </style>
+
+    
     """,
     unsafe_allow_html=True
 )
 
 # Display the animated welcome message
 st.markdown("<div class='welcome-message'>Welcome to Omni GPT!</div>", unsafe_allow_html=True)
+## Custom CSS for spinner
+st.markdown("""
+  <style>
+  div.stSpinner {
+    text-align:center;
+    align-items: center;
+    justify-content: center;
+  }
+  </style>""", unsafe_allow_html=True)
+  
 
 # Placeholder for main content
 main_placeholder = st.empty()
@@ -86,18 +102,18 @@ user_query = main_placeholder.text_input(
 col1, col2, col3, col4 = st.columns([2.5, 2, 2.4, 3.7], vertical_alignment="center", gap="small")
 
 with col2:
-    st.button('Analyze Text', key='analyze_text', help='Click to analyze the text.')
+    analyze_button=st.button('Analyze Text', key='analyze_text', help='Click to analyze the text.')
 
 with col3:
     st.button('Generate Image', key='generate_image', help='Click to generate an image.')
 
 # Sidebar elements with titles and selectboxes
 st.sidebar.title("Model for Text Analysis")
-selected_text_model = st.sidebar.selectbox("Select a Text Model", ["Model 1", "Model 2"], 
+selected_text_model = st.sidebar.selectbox("Select a Text Model", ["Gemini-1.5-Flash", "Model 2"], 
                                                label_visibility="hidden")
 
 st.sidebar.title("Model for QAs")
-selected_qa_model = st.sidebar.selectbox("Select a QA Model", ["Model 1", "Model 2"], 
+selected_qa_model = st.sidebar.selectbox("Select a QA Model", ["Gemini-1.5-Flash", "Model 2"], 
                                              label_visibility="hidden")
 
 st.sidebar.title("Model for Generating Image")
@@ -107,3 +123,27 @@ selected_image_model = st.sidebar.selectbox("Select an Image Generation Model", 
 st.sidebar.title("Upload a File")
 uploaded_document = st.sidebar.file_uploader("Upload a PDF or Image File", type=["pdf", "txt", "jpeg", "png", "jpg"],
                                                   help="Upload a PDF or text file for analysis and an image for OCR.", label_visibility="hidden")
+
+
+## Check if the user has clicked the analyze button and generate text bases on 
+if user_query and analyze_button:
+    if selected_text_model=="Gemini-1.5-Flash":
+        loading_placeholder = st.empty()
+        with st.spinner("Generating Response"):
+            response=google_genai_text(user_query)
+        # Display the response
+            st.write("\n"*3)
+            st.write(response)
+    if selected_text_model=="Model 2":
+        pass
+
+if user_query and uploaded_document:
+    if selected_qa_model=="Gemini-1.5-Flash":
+        loading_placeholder = st.empty()
+        with st.spinner("Generating Response"):
+            response=google_genai_image(user_query)
+        # Display the response
+            st.write("\n"*3)
+            st.write(response)
+    if selected_qa_model=="Model 2":
+        pass
---
+++ code/text_models.py
@@ -0,0 +1,30 @@
+import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
+from dotenv import load_dotenv
+import os
+
+def google_genai_text(quesion:str="What is the capital of France?"):
+    ## Load the google api key
+    env_keys=load_dotenv("./keys.env")
+    google_env_key=os.getenv('GOOGLE_API_KEY')
+        ## Configure the generation config
+    generation_config = genai.GenerationConfig(
+            max_output_tokens=1000, temperature=0.1
+    )
+        ## Configure the safety settings
+    safety_settings={
+        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE
+    }
+    ## Configure the google api
+    genai.configure(api_key=google_env_key)
+    ## Load the model
+    model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+    ## Generate the content
+    response=model.generate_content(quesion,generation_config=generation_config,safety_settings=safety_settings)    
+    return response.text
+
+
+
